c88a24a1df2ba58096b9d52a228cd335f05cffb6
Jerry Zou <jerry.zry@outlook.com>
2015-11-23 21:16:32 +0800

    insert result data into mongodb

--- main.js
+++ main.js
@@ -2,10 +2,13 @@
 
 let fs = require('fs')
   , exec = require('exec')
+  , mongodb = require('mongodb')
+  , MongoClient = mongodb.MongoClient
 
 let submitCurl = fs.readFileSync('./scripts/submit.sh', 'utf8')
   , checkCurl = fs.readFileSync('./scripts/check.sh', 'utf8')
 
+// execute curl program
 exec(submitCurl, function (err, result, code) {
   if (err instanceof Error) throw err;
   let res = JSON.parse(result)
@@ -15,6 +18,10 @@ exec(submitCurl, function (err, result, code) {
       exec(acturalCheckCurl, function (err, result, code) {
         let res = JSON.parse(result)
         if (res.state == "SUCCESS") {
+          dbInsert({
+            timestamp: Date.now(),
+            runtime: res.status_runtime
+          })
           console.log(res.status_runtime)
         } else {
           setTimeout(check, 200)
@@ -23,3 +30,17 @@ exec(submitCurl, function (err, result, code) {
     }())
   }
 })
+
+function dbInsert(obj) {
+  let url = 'mongodb://localhost:27017/leetcode'
+  MongoClient.connect(url, function (err, db) {
+    console.log('Connected correctly to server')
+    
+    let collection = db.collection('benchmark')
+    collection.insertMany([obj], function (err, result) {
+      if (err) console.log(err)
+      else console.log("Insert Successfully.")
+      db.close()
+    })
+  })
+}
